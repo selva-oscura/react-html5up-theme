54cfc4f65e46aef10970e04d20007faf98140d32
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-07 22:14:09 -0700

    corrected typo (es2015, not es2016 for presets)

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 				loader: 'babel-loader',
 				exclude: /(node_modules)/,
 				query: {
-					presets: ['react', 'es2016']
+					presets: ['react', 'es2015']
 				}
 			}
 		]
