da8f8ee8d0ad0caebc607152e273adba54431392
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-07 21:49:59 -0700

    fleshed out App stub

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+	render(){
+		return(
+			<div>React Entry Point</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getELementById('root'));
