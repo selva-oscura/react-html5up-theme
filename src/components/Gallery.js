e01ee94e0e30f4b57d9660a11c9d9873433cb307
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-08 22:54:45 -0700

    moved gallery code from App to new Gallery component

---
+++ src/components/Gallery.js
@@ -0,0 +1,162 @@
+import React from 'react';
+
+const Gallery = (props) => {
+	return(
+		<section className="wrapper style1 align-center">
+			<div className="inner">
+				<h2>Massa sed condimentum</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum.</p>
+			</div>
+
+				<div className="gallery style2 medium lightbox onscroll-fade-in">
+					<article>
+						<a href="images/gallery/fulls/01.jpg" className="image">
+							<img src="images/gallery/thumbs/01.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Ipsum Dolor</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/02.jpg" className="image">
+							<img src="images/gallery/thumbs/02.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Feugiat Lorem</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/03.jpg" className="image">
+							<img src="images/gallery/thumbs/03.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Magna Amet</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/04.jpg" className="image">
+							<img src="images/gallery/thumbs/04.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Sed Tempus</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/05.jpg" className="image">
+							<img src="images/gallery/thumbs/05.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Ultrices Magna</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/06.jpg" className="image">
+							<img src="images/gallery/thumbs/06.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Sed Tempus</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/07.jpg" className="image">
+							<img src="images/gallery/thumbs/07.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Ipsum Lorem</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/08.jpg" className="image">
+							<img src="images/gallery/thumbs/08.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Magna Risus</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/09.jpg" className="image">
+							<img src="images/gallery/thumbs/09.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Tempus Dolor</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/10.jpg" className="image">
+							<img src="images/gallery/thumbs/10.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Sed Etiam</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/11.jpg" className="image">
+							<img src="images/gallery/thumbs/11.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Magna Lorem</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+					<article>
+						<a href="images/gallery/fulls/12.jpg" className="image">
+							<img src="images/gallery/thumbs/12.jpg" alt="" />
+						</a>
+						<div className="caption">
+							<h3>Ipsum Dolor</h3>
+							<p>Lorem ipsum dolor amet, consectetur magna etiam elit. Etiam sed ultrices.</p>
+							<ul className="actions">
+								<li><span className="button small">Details</span></li>
+							</ul>
+						</div>
+					</article>
+				</div>
+
+		</section>
+	)
+}
+
+export default Gallery;
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Footer from './Footer';
+import Gallery from './Gallery';
 import Splash from './Splash';
 import Spotlight from './Spotlight';
 
 export {
 	Footer,
+	Gallery,
 	Splash,
 	Spotlight,
 }
