d316a2022f55f8f8e4917a8ddfe0090f19bbace5
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-08 22:55:48 -0700

    moved head section for Gallery into own component (SectionHead)

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import SectionHead from './SectionHead';
 
 const Gallery = (props) => {
 	return(
 		<section className="wrapper style1 align-center">
-			<div className="inner">
-				<h2>Massa sed condimentum</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum.</p>
-			</div>
+
+			<SectionHead />
 
 				<div className="gallery style2 medium lightbox onscroll-fade-in">
 					<article>
---
+++ src/components/SectionHead.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SectionHead = (props) => {
+	return (
+		<div className="inner">
+			<h2>Massa sed condimentum</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum.</p>
+		</div>
+	)
+}
+
+export default SectionHead;
