63262e0d8b6b850d6f9ebcdfbb1ebf559939ef0d
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-10 16:37:43 -0700

    replaced hardcoded form data with passing of prop data to FormElement

--- src/components/FormArea.js
+++ src/components/FormArea.js
@@ -1,26 +1,14 @@
 import React from 'react';
+import FormElement from './FormElement';
 
 const FormArea = (props) => {
+	console.log("props from FormArea", props)
 	return (
 		<section className="wrapper style1 align-center">
 			<div className="inner medium">
-				<h2>Get in touch</h2>
+				<h2>{props.callToAction}</h2>
 				<form method="post" action="#">
-					<div className="field half first">
-						<label htmlFor="name">Name</label>
-						<input type="text" name="name" id="name" value="" />
-					</div>
-					<div className="field half">
-						<label htmlFor="email">Email</label>
-						<input type="email" name="email" id="email" value="" />
-					</div>
-					<div className="field">
-						<label htmlFor="message">Message</label>
-						<textarea name="message" id="message" rows="6"></textarea>
-					</div>
-					<ul className="actions">
-						<li><input type="submit" name="submit" id="submit" value="Send Message" /></li>
-					</ul>
+					{props.form.map((formElement, i) => (<FormElement key={i} formElement={formElement} />))}
 				</form>
 
 			</div>
