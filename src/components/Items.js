280f5b0128da4ec3084b92b7b0a128a2fedc26ef
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-09 22:02:27 -0700

    moved hard-coded Items grid code to ItemsBody

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SectionHead from './SectionHead';
+import ItemsBody from './ItemsBody';
 
 const Items = (props) => {
 	return(
@@ -8,70 +9,8 @@ const Items = (props) => {
 				title={props.sectionHeadTitle}
 				text={props.sectionHeadText}
 			/>
-			<div className="inner">
-				<div className="items style1 medium onscroll-fade-in">
-					<section>
-						<span className="icon style2 major fa-diamond"></span>
-						<h3>Lorem</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-save"></span>
-						<h3>Ipsum</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-bar-chart"></span>
-						<h3>Dolor</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-wifi"></span>
-						<h3>Amet</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-cog"></span>
-						<h3>Magna</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-paper-plane"></span>
-						<h3>Tempus</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-desktop"></span>
-						<h3>Aliquam</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-refresh"></span>
-						<h3>Elit</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-hashtag"></span>
-						<h3>Morbi</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-bolt"></span>
-						<h3>Turpis</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-envelope"></span>
-						<h3>Ultrices</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-					<section>
-						<span className="icon style2 major fa-leaf"></span>
-						<h3>Risus</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
-					</section>
-				</div>
-			</div>
+			<ItemsBody />			
+
 		</section>
 	)
 }
---
+++ src/components/ItemsBody.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const ItemsBody = (props) => {
+	return (
+		<div className="inner">
+			<div className="items style1 medium onscroll-fade-in">
+				<section>
+					<span className="icon style2 major fa-diamond"></span>
+					<h3>Lorem</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-save"></span>
+					<h3>Ipsum</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-bar-chart"></span>
+					<h3>Dolor</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-wifi"></span>
+					<h3>Amet</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-cog"></span>
+					<h3>Magna</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-paper-plane"></span>
+					<h3>Tempus</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-desktop"></span>
+					<h3>Aliquam</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-refresh"></span>
+					<h3>Elit</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-hashtag"></span>
+					<h3>Morbi</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-bolt"></span>
+					<h3>Turpis</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-envelope"></span>
+					<h3>Ultrices</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+				<section>
+					<span className="icon style2 major fa-leaf"></span>
+					<h3>Risus</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dui turpis, cursus eget orci amet aliquam congue semper. Etiam eget ultrices risus nec tempor elit.</p>
+				</section>
+			</div>
+		</div>
+	)
+}
+
+export default ItemsBody;
