9139df2d89992dffa9cf8e779597468bc237f83d
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-09 22:38:09 -0700

    imported/exported FormArea

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Footer from './Footer';
+import FormArea from './FormArea';
 import Gallery from './Gallery';
 import Items from './Items';
 import Splash from './Splash';
@@ -6,6 +7,7 @@ import Spotlight from './Spotlight';
 
 export {
 	Footer,
+	FormArea,
 	Gallery,
 	Items,
 	Splash,
