4063ef33a63b55ee1925f201c5725ae1c2d3bf5c
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-09 21:38:16 -0700

    imported and exported Items in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Footer from './Footer';
 import Gallery from './Gallery';
+import Items from './Items';
 import Splash from './Splash';
 import Spotlight from './Spotlight';
 
 export {
 	Footer,
 	Gallery,
+	Items,
 	Splash,
 	Spotlight,
 }
