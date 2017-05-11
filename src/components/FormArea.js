1c83a46d7e81336a27288ad8089d18a968c5d508
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-10 23:19:10 -0700

    passed formState, updateFormState, submitMailForm, and formElement data to FormElement

--- src/components/FormArea.js
+++ src/components/FormArea.js
@@ -2,15 +2,21 @@ import React from 'react';
 import FormElement from './FormElement';
 
 const FormArea = (props) => {
-	console.log("props from FormArea", props)
 	return (
 		<section className="wrapper style1 align-center">
 			<div className="inner medium">
 				<h2>{props.callToAction}</h2>
 				<form method="post" action="#">
-					{props.form.map((formElement, i) => (<FormElement key={i} formElement={formElement} />))}
+					{props.form.map((formElement, i) => (
+						<FormElement 
+							key={i}
+							formState={props.formState}
+							updateFormState={props.updateFormState}
+							submitMailForm={props.submitMailForm}
+							formElement={formElement}
+						/>
+					))}
 				</form>
-
 			</div>
 		</section>
 	)
