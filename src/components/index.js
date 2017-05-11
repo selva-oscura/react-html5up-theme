22c84473a98895fa696911676aefc52b5629d54b
Carol St.Louis <1.selva.oscura@gmail.com>
2017-05-10 23:22:41 -0700

    made Errors and Messages available to App

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,19 @@
+import Errors from './Errors';
 import Footer from './Footer';
 import FormArea from './FormArea';
 import Gallery from './Gallery';
 import Items from './Items';
+import Messages from './Messages';
 import Splash from './Splash';
 import Spotlight from './Spotlight';
 
 export {
+	Errors,
 	Footer,
 	FormArea,
 	Gallery,
 	Items,
+	Messages,
 	Splash,
 	Spotlight,
 }
